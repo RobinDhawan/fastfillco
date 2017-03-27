3921b4339573bfaf48e0b09e4f31ddebd98754c8
Robin Dhawan <robin.dhawan@aricent.com>
2017-03-27 13:03:39 +0530

    test

--- src/main/java/com/nurds/fastfillco/model/MasterClass.java
+++ src/main/java/com/nurds/fastfillco/model/MasterClass.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
@@ -32,6 +34,7 @@ public class MasterClass extends ResponseObject implements Serializable {
  
   @Id
   @NotNull
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
   
   @NotNull
